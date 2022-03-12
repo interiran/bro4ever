0f709f4a6d58226a8e4beddbd262b51885db078b
interiran <96937017+interiran@users.noreply.github.com>
2022-03-12 20:57:21 +0330

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,1 @@
+ffmpeg -i "http://tv.rapidswiss.hitv.one:80/live/JYlFsFHLo9099/HwQnhaVkJS099/15197.m3u8" -vcodec copy -acodec aac -f flv "rtmps://dc4-1.rtmp.t.me/s/1698007507:VDEaxuyAQthoyj5W8KzVIw"
