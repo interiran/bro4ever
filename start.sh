9aaf27f4dc60366b9fefd0ea6df3ecf8a64437a5
interiran <96937017+interiran@users.noreply.github.com>
2022-03-17 00:13:21 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-ss-local -c local.json & proxychains4 -f proxychains.conf ffmpeg -i "http://tv.rapidswiss.hitv.one/live/JYlFsFHLo9099/HwQnhaVkJS099/15733.m3u8" -c:v libx264 -preset: veryfast -tune zerolatency -b:v 4800k -minrate 4000k -maxrate 4800k -bufsize 4800k -r 30 -g 60 -force_key_frames "expr:gte(t,n_forced*2)" -c:a copy -f flv "rtmps://dc4-1.rtmp.t.me/s/1698007507:VDEaxuyAQthoyj5W8KzVIw"
+ss-local -c local.json & proxychains4 -f proxychains.conf ffmpeg -i "https://ky-iptv.com/live/hey454545/12345645/1248327.m3u8" -c:v libx264 -preset: veryfast -tune zerolatency -b:v 4800k -minrate 4000k -maxrate 4800k -bufsize 4800k -r 30 -g 60 -force_key_frames "expr:gte(t,n_forced*2)" -c:a copy -f flv "rtmps://dc4-1.rtmp.t.me/s/1698007507:VDEaxuyAQthoyj5W8KzVIw"
 
